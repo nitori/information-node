68d5c5f56b2706081c609c3f6464d3a86dac7700
Jonas Thiem <jonasthiem@googlemaill.com>
2015-08-16 06:36:24 +0200

    More work on cli, adding some README contents

--- README.txt
+++ README.txt
@@ -1,3 +1,76 @@
 
-This project is heavily work in progress. Don't use.
+== Information nodes ==
+This project offers a new concept of **information nodes** for keeping your
+data synchronized between various locations (cloud storage if you will) and
+safe with encryption.
+
+It supports POSIX-systems (Linux, BSD, Mac OS X, ..) and Microsoft Windows.
+
+The main differences to Dropbox or syncthing are:
+
+* no central server, instead peer-to-peer (similar to syncthing)
+
+* support for non-file data like e-mails, IM, ... including gateways that
+  automatically fetch your e-mails, messages, etc into a node and sync it into
+  your network.
+
+* graphical viewer that browses and searches all the data easily
+
+* built-in support for encryption, including storing data on a remote location
+  just as fully asynchronously encrypted blob that can only be decrypted at
+  your main site
+
+Please note due to the advanced featureset (non-file support for e-mails, ..
+and encryption), this tool is slightly more work to learn and use than
+the "easy" choices like Dropbox or syncthing.
+
+However, all command line tools have extensive documentation and detailed
+commands allowing a very hands on approach in managing your nodes and making
+sure everything is synchronized the way you want. You can also use the
+graphical viewer to assist you with managing your nodes.
+
+=== Why? ===
+The philosophy of information nodes is to allow you to move ALL your data,
+not just your files, including all your communication activity, into
+possibly encrypted nodes. In days where all sorts of attackers and
+agencies want to spy onto your data, this should help you set up a trusted
+storage network with decryption only possible from your local trusted laptop
+or with a secured key file you have on your local usb stick and not in the
+cloud.
+
+== How to get started / INSTALL ==
+
+** WARNING THIS PROJECT IS HIGHLY UNFINISHED. DONT USE **
+
+=== Windows ===
+
+1. Install latest Python 3 release for windows: https://www.python.org/downloads/windows/
+
+2. Download this project as .zip if you haven't already: https://github.com/information-node/information-node/archive/master.zip
+
+3. Run viewer.bat and enter the python installation path when it asks you
+
+=== Linux / BSD / Mac OS X ... ===
+
+Download this project with git or as .zip and extract it in some folder.
+Then, the easiest way of install is:
+
+```bash
+sudo python3 setup.py install
+```
+(inside the information-node project folder)
+
+Afterwards, you can use the included tools directly from the command line.
+Enter the following to open up the graphical viewer:
+
+```bash
+inode-viewer
+```
+
+== What exact data types and protocols are supported? ==
+At this moment, the following data types and protocols can be handled by
+an information node network:
+
+* regular files (similar to Dropbox/syncthing)
+
 
---
+++ informationnode/helper.py
@@ -0,0 +1,160 @@
+
+'''
+information-node - an advanced tool for data synchronization
+Copyright (C) 2015  information-node Development Team (see AUTHORS.md)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import argparse
+import os
+import re
+import sys
+import textwrap
+
+class DoubleLineBreakFormatter(argparse.HelpFormatter):
+    """ Retains double line breaks/paragraphs """
+    def _split_lines(self, text, width):
+        return self._fill_text(text, width, "").splitlines(False)
+
+    @staticmethod
+    def _static_fill_text(t, width, indent):
+        t = " ".join([s for s in t.replace("\t", " ").strip("\t ").split(" ")\
+            if len(s) > 0]).replace("\n ", "\n").replace(" \n", " ")
+        ts = re.sub("([^\n])\n([^\n])", "\\1 \\2", t).split("\n\n")
+        result = [textwrap.fill(paragraph, width,
+            initial_indent=indent, subsequent_indent=indent)\
+            for paragraph in ts]
+        return "\n\n".join(result).replace("[b]",
+        "\033[1m").replace("[/b]", "\033[21m")
+
+    def _fill_text(self, t, width, indent):
+        return self._static_fill_text(t, width, indent)
+
+def check_if_node_dir(path, allow_new=False):
+    """ Check if the given directory is a valid node path.
+        If allow_new is True, an empty directory will also result in a
+        positive check.
+
+        Returns (result, msg)
+
+        result: True if check succeeded, otherwise False
+        msg: string with an error message in case result was False
+    """
+    if os.path.exists(path):
+        if not os.path.isdir(path):
+            return (False, "specified node folder is " +\
+                "not a directory")
+        contents = os.listdir(path)
+        if len(contents) == 0:
+            # empty directory.
+            if allow_new:
+                return True
+            else:
+                return (False, "specified node folder is " +\
+                    "not a valid information node")
+        else:
+            # check if a valid information node:
+            if not os.path.exists(os.path.join(path, "storage")) or \
+                    not os.path.exists(os.path.join(path, "identity.secret")):
+                return (False, "specified node folder is " +\
+                    "not a valid information node")
+            return (True, None)
+    else:
+        if not allow_new:
+            return (False, "specified node folder " +\
+                "doesn't exist")
+
+        # clean up path a bit:
+        path = os.path.normpath(os.path.abspath(path))
+        if path.startswith("//"):
+            path = path[1:]
+        while path.endswith("/"):
+            path = path[:-1]
+        while (platform.system().lower() == "windows" and\
+                path.endswith("\\")):
+            path = path[:-1]
+
+        # find out whether the parent dir exists:
+        base_dir = os.path.dirname(path)
+        if not os.path.exists(base_dir) or not os.path.isdir(base_dir):
+            return (False, "cannot create node at " +\
+                "specified location because parent directory doesn't" +\
+                " exist: " + str(base_dir))
+
+        # create directory as new empty dir:
+        try:
+            os.mkdir(path)
+        except OSError as e:
+            return (False, "cannot create node at " +\
+                "specified location: " + str(e))
+        return (True, None)
+
+def check_if_node_runs(node_folder):
+    """ Check if the given node runs. Returns a tuple with
+        (result, msg)
+
+        result is: True (node runs), False (node doesn't run), None (error)
+        msg: in case of an error, contains a string with the error details.
+             If the node is running, this contains the process id.
+    """
+    # check if pid file exists:
+    if not os.path.exists(os.path.join(node_folder, "pidfile")):
+        return (False, None)
+
+    # open it up to check for the running process:
+    try:
+        f = open(os.path.join(node_folder, "pidfile"), "rb")
+    except OSError:
+        return (None, "failed to read pidfile")
+    contents = f.read()
+    f.close()
+
+    # check if valid pid:
+    try:
+        pidnumber = int(contents)
+    except ValueError:
+        pidnumber = None
+    if pidnumber == None or pidnumber < 1:
+        return (None, "pidfile has invalid contents. "+\
+            "Check if this node has already a process running, and if "+\
+            "you are sure this is not the case, remove the pidfile.")
+
+    # check if a process with this pid is running:
+    def check_process_running(pid):
+        if platform.system() == "windows":
+            PROCESS_QUERY_INFROMATION = 0x1000
+            processHandle = ctypes.windll.kernel32.OpenProcess(
+                PROCESS_QUERY_INFROMATION, 0, pid)
+            if processHandle == 0:
+                return False
+            else:
+                ctypes.windll.kernel32.CloseHandle(processHandle)
+            return True 
+        else:
+            try:
+                os.kill(pid, 0)
+            except OSError:
+                return False
+            else:
+                return True
+
+    if check_process_running(pidnumber):
+        return (True, pidnumber)
+    
+    print("warning: removing stale information node " +\
+        "pidfile with pid: " + str(pidnumber), file=sys.stderr)
+    os.remove(os.path.join(node_folder, "pidfile"))
+    return (False, None)
+
