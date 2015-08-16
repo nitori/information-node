9bc26fb896f64bfbd1fd4f11bb62ce2c5e4e3ea9
Jonas Thiem <jonasthiem@googlemail.com>
2015-08-16 08:22:12 +0200

    Working on the viewer.bat launcher

--- viewer.bat
+++ viewer.bat
@@ -1,11 +1,14 @@
-
-IF NOT EXIST "%~dp\python" (
-    ECHO INSTALL: Download Python 3...
+@echo off
+IF NOT EXIST %~dp0\python (
+    IF EXIST %~dp0\python.msi (
+        DEL python.msi
+    )
+    ECHO PLEASE WAIT !!!   Downloading Python 3...
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.4.3/python-3.4.3.amd64.msi', 'python.msi')"
-    ECHO INSTALL: Running Python 3 install...
-    msiexec /i python.msi ALLUSERS="" TARGETDIR="%~dp0\python"
+    ECHO PLEASE WAIT !!!   Running Python 3 install...
+    msiexec /i python.msi ALLUSERS="" TARGETDIR="%~dp0\python" /q
     IF ERRORLEVEL 1 (
-        IF NOT EXIST "%~dp-\python" (
+        IF NOT EXIST %~dp0\python (
             ECHO FATAL ERROR: it appears Python 3 failed to install. Try to install it manually.
             PAUSE
             EXIT /B 1
@@ -13,4 +16,8 @@ IF NOT EXIST "%~dp\python" (
     )
 )
 
-ECHO INSTALL: Launching install script...
+ECHO Launching viewer...
+SET PATH=%~dp0\;%~dp0\python\;%PATH%
+SET PYTHONPATH=%~dp0\
+python.exe inode-viewer
+pause
