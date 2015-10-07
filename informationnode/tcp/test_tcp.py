bb38776ca1f87d774dad6fe16d883add4805cc8d
Jonas Thiem <jonasthiem@googlemaill.com>
2015-10-07 19:32:55 +0200

    Making first minimal fail test run

---
+++ informationnode/tcp/test_tcp.py
@@ -0,0 +1,16 @@
+
+import unittest
+
+class TCPTest(unittest.TestCase):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_main(self):
+        self.assertEqual(1, 2)
+
